cee3c1c336146f20a0eb36e62d61ecf41ad4f39f
Shruti Kaur <ich524@usask.ca>
2024-12-08 12:21:32 -0600

    We are able to now get the result. Previously, error was coming because of IFS='\n' in the code. Now, I am able to better understand it and it is able to generate the outputs that I need

--- Valid_Phone_Numbers/result.sh
+++ Valid_Phone_Numbers/result.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Storing the lines inside a bash array called "phonenumbers"
-IFS="\n" read -d '' -r -a phonenumbers < file.txt
+IFS=$'\n' read -d '' -r -a phonenumbers < file.txt
 
 for lines in "${phonenumbers[@]}"; do
-	if [[ "$lines" =~ ^\([1-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]]; then
-		echo "$lines"
-	elif [[ "$lines" =~ ^[1-9]{3}-[0-9]{3}-[0-9]{4}$ ]]; then
-		echo "$lines"
-	fi
+   if [[ "$lines" =~ ^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]]; then
+	echo "$lines"
+   elif [[ "$lines" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]]; then
+	echo "$lines"
+   fi
 done
+
+
+
