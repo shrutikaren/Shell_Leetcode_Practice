f2f0a24c8a9c16cfe660cf081ce755051aa47238
Shruti Kaur <ich524@usask.ca>
2024-12-08 11:26:31 -0600

    Removed spaces between the variables and the equal sign. Also, having an error with the line being printed as it is printing the tenth line but it is a number 5 instead of 'line 10'

--- Tenth_Line/result.sh
+++ Tenth_Line/result.sh
@@ -2,11 +2,24 @@
 
 # Take all of the lines into a bash array 
 IFS=$'\n' read -d '' -r -a arraylines < file.txt
-echo "${arraylines[@]}"
+
+TARGET=9
+CURRENT_COUNT=0
 
 # Iterate through the array to obtain the 10th line
+for line in ${arraylines[@]}; do
+	# If we reached line number 10
+	if [ "$CURRENT_COUNT" -eq "$TARGET" ]; then
+		# If the line actually does exist 
+		echo $line 
+		
+		# Once we have reached the tenth line, our goal is completed
+		# So we are going to go ahead and exit out of our for-loop
+		# basically
+		exit 0
+	fi
+	CURRENT_COUNT=$((CURRENT_COUNT + 1))
+done
 
-# Produce the 10th line if it's available 
-
-# If not, produce nothing 
-
+# If nothing get's printed out, we print an empty line 
+echo ""
