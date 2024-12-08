4ddafbcf78e986f6cc7eb16dc669219c922b2923
Shruti Kaur <ich524@usask.ca>
2024-12-08 12:04:21 -0600

    The code doesn't seem to produce any errors but it is not printing out any output even though the patterns were taken into consideration

---
+++ Valid_Phone_Numbers/result.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Storing the lines inside a bash array called "phonenumbers"
+IFS="\n" read -d '' -r -a phonenumbers < file.txt
+
+for lines in "${phonenumbers[@]}"; do
+	if [[ "$lines" =~ ^\([1-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]]; then
+		echo "$lines"
+	elif [[ "$lines" =~ ^[1-9]{3}-[0-9]{3}-[0-9]{4}$ ]]; then
+		echo "$lines"
+	fi
+done
